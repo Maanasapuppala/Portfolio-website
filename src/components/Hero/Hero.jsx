4c953153b70d43b37d7060e88e55b504d07853e0
Manasa <manasapuppala9@gmail.com>
2025-05-04 18:13:01 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
    <section className={styles.container}>
     <div className={styles.content}>
         <h1 className={styles.title}> Hi I'm Manasa</h1>
-        <p className={styles.description}>I'm a fresher.I'm good at front-end. 
+        <p className={styles.description}> As a passionate Frontend Developer,I specialize in building responsive and interactive web applications using HTML,CSS,JavaScript and React.js.I thrive on creating seamless user experience.Explore my projects below to see how I combie functionality with creative to deliver impactful solutions. 
             Reach out if you'd like to learn more!</p>
         <a href='mailto:manasapuppala9@gmail.com' className={styles.contactBtn}>
             Contact Me</a>
